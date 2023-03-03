9976cb8c150b4e2ab56c857ec05c5ac6bf03c924
Vincent <kemeny.vince@outlook.hu>
2023-03-03 20:52:03 +0100

    "Bejelentkezés" és regisztráció link megjavítva

--- alkalomadtan/alkalomadtan/settings.py
+++ alkalomadtan/alkalomadtan/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# ez lesz az alapértelmezett url kezelő (megkeresi a fájl amiben az url címek vannak)
 ROOT_URLCONF = 'alkalomadtan.urls'
 
 TEMPLATES = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'hu-uhu'
 
 TIME_ZONE = 'UTC'
 

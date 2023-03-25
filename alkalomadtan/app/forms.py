31c28e56aefd00d0e57c85ac85bfdf8a4937ce04
Kemény Vince <93611221+KVince002@users.noreply.github.com>
2023-03-25 23:09:22 +0100

    Kommentek rendezése 1

--- alkalomadtan/app/forms.py
+++ alkalomadtan/app/forms.py
@@ -9,33 +9,17 @@ from django.core.exceptions import ValidationError
 from django.forms.fields import EmailField
 
 # kollektív fiók regisztrálás
-class Regisztralas(UserCreationForm):   
-    # felhasznaloNev = forms.CharField(label="Felhasználónév", min_length=3, max_length=150)
+class Regisztralas(UserCreationForm):
+    # komment tisztítás
     first_name = forms.CharField(label="Előnév", max_length=150, required=True)
     last_name = forms.CharField(label="Utónév", max_length=150, required=True)
-    # email = forms.EmailField(label="Email cím", widget=forms.EmailInput)
-    # jelszo1 = forms.CharField(label="Jelszava", widget=forms.PasswordInput)
-    # jelszo2 = forms.CharField(label="Jelszava még egyszer", widget=forms.PasswordInput)
 
     class Meta:
         model = UserCreationForm.Meta.model
-        fields = UserCreationForm.Meta.fields + ("first_name", "last_name")
+        # Mivel a UserCreationForm egy már meglévő osztály, de mi szeretnénk hoztáadni egy pár új mezőt. A user tábla már tartalmazza ezt a két mezőt csak alapértelmezetten nem tartalmazza az űrlap.
+        fields = UserCreationForm.Meta.fields + ("first_name", "last_name") # type: ignore
 
     # ezekkel a függvényekkel olvassuk és ellenőrizzük az adatokat amiket a From filed-ekből szerzünk meg
-    # def clean_felhasznalo(self):
-    #     felhasznaloNev = self.cleaned_data['felhasznaloNev'].lower()
-    #     keres = User.objects.filter(username=felhasznaloNev)
-    #     if keres.count():
-    #         raise ValidationError("Ez a felhasználónév már létezik!")
-    #     return felhasznaloNev
-
-    # def clean_email(self):
-    #     email = self.cleaned_data["email"].lower()
-    #     keres = User.objects.filter(email=email)
-    #     if keres.count():
-    #         raise ValidationError("Ez az email címmel már regisztráltak!")
-    #     return email
-
     def clean_eloNev(self):
         eloNev = self.cleaned_data["eloNev"].lower()
         return eloNev
@@ -43,28 +27,6 @@ class Regisztralas(UserCreationForm):
     def clean_utoNev(self):
         utoNev = self.cleaned_data["utoNev"].lower()
         return utoNev
-    # ezzel lehet ellenőrizni hogy a két jelszó mező egyezik-e
-
-    # def clean_jelszomegint(self):
-    #     jelszo1 = self.cleaned_data["jelszo1"]
-    #     jelszo2 = self.cleaned_data["jelszo2"]
-    #     if jelszo1 != jelszo2:
-    #         raise ValidationError("A jelszavak nem egyeznek!")
-    #     # if jelszo1 != "" or jelszo2 != "":
-    #     #     raise ValidationError("Biztos hogy kitöltötte a két jelszőmezőt?")
-    #     return jelszo2
-
-    # def Mentes(self):
-    #     felhasznalo = User.objects.create_user(
-    #         username = self.clean_felhasznalo(),
-    #         email = self.clean_email(),
-    #         password = self.clean_jelszomegint(),
-    #     )
-    #     felhasznalo.first_name = self.clean_eloNev()
-    #     felhasznalo.last_name = self.clean_utoNev()
-    #     felhasznalo.save()
-        
-        return felhasznalo
 
 class JelentkezesFormModel(ModelForm):
     class Meta:
@@ -96,8 +58,11 @@ class BejelentkezesForm(forms.Form):
 
 class MunkaFrom(ModelForm):
     class Meta:
+        # melyik táblaát használja
         model = Munka
+        # mely mezőkkel dolgozzon
         fields = "__all__"
+        # mely menzőkkel NE dolgozzon
         exclude = ["publikalo", "katt"]
 
     nev = forms.CharField(label="Munka neve", max_length=250)

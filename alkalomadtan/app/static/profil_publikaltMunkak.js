7d9030806787eb9567c0a535da17adbcf0463833
Kemény Vince <93611221+KVince002@users.noreply.github.com>
2023-04-24 18:36:13 +0200

    profil munka megjelenítő js frissítve 🛑

--- alkalomadtan/app/static/profil_publikaltMunkak.js
+++ alkalomadtan/app/static/profil_publikaltMunkak.js
@@ -1,6 +1,10 @@
 // globális változók
 // html collection
 var munkak = document.getElementById("mindenMunka");
+// minden munkát ami megjelenni a django által eltárolja
+var mindenMunkak_Tomb = mindenMunkak();
+// a jelentlegi munkát adja vissza egy object-ként
+let mostaniMunka = munkaMutatoStatusz();
 
 // Minden munka az oldalon
 function mindenMunkak() {
@@ -16,46 +20,60 @@ function mindenMunkak() {
 
 // minden látható munkát töröl
 function osszesMunkaTorlo(munkakTomb) {
-    // ismeretlen hiba miatt biztosra megyek
-    for (let i = 0; i < munkakTomb.length; i++) {
-        console.log("munka törlés: ", i, " ez a ", munkakTomb[i])
-        munkak.removeChild(munkakTomb[i]);
+    try {
+        console.log(munkak.childElementCount)
+        for (let i = 0; i < munkak.childElementCount; i++) {
+            console.log("munka törlés: ", i, " ez a ", munkakTomb[i])
+            munkak.removeChild(munkakTomb[i]);
 
+        }
     }
+    catch (ex) {
+        console.log("!")
+        console.log(ex);
+    }
+
 }
 
 // státusz ellenőrző (melyik munka megjelenítve)
 function munkaMutatoStatusz() {
     /*
     ez majd egy string-et ad majd vissza, ami az adott div-nek az id-ja.
-    Azon a logkián alapulva hogy úgy is csak egy munka lesz majd megjelenítve, így csak egy child lesz munkak div-ben
+    Azon a logkián alapulva hogy úgy is csak egy munka lesz majd megjelenítve, így csak egy child lesz munkak div-ben.
+    A visszatérési értéke egy szótár (dict) lesz
     */
-    return munkak.children[0].id
+    return { key: munkak.children[0].id, value: munkak.children[0] };
 }
 
 // első megjelenítendő munkát ad vissza
 /*
-    úgytűnik ez a függvén a leges legutóbbi munkát mutatja
+    úgytűnik ez a függvény a leges legutóbbi munkát mutatja
 */
 function elsoMunka(munkakTomb) {
     munkak.appendChild(munkakTomb[0]);
 }
-
 /*
     az alábbi függvények lesznek amik a nyilakat vezérlik a megjelenítendő munkákat
 */
 // előre nyíl (jobbra mutató)
 function eloreNyil(munkakTomb) {
-    let mostaniMunka = munkaMutatoStatusz();
-
+    // evvel tudom hogy mi a most megjelenített munka
+    console.log("lapozás előre");
+    if (munkakTomb.length > 1) {
+        // törölje a most megjelenő munkát
+        munkak.removeChild(mostaniMunka.value);
+    }
 }
 
-// függvény meghívások
-window.addEventListener("DOMContentLoaded", (event) => {
-    console.log("DOM fully loaded and parsed");
-    //függvény hívások
-    var mindenMunkak_Tomb = mindenMunkak();
-    osszesMunkaTorlo(mindenMunkak_Tomb);
+// gombok
+document.getElementById("lapozoElore").onclick = eloreNyil(mindenMunkak_Tomb);
 
-    elsoMunka(mindenMunkak_Tomb)
-});
+// függvény meghívások
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     console.log("DOM fully loaded and parsed");
+
+// });
+
+//függvény hívások
+osszesMunkaTorlo(mindenMunkak_Tomb);
+elsoMunka(mindenMunkak_Tomb)

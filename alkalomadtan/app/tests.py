b43f36705f3904e1d360cebec5703412ffcf6a69
Kemény Vince <93611221+KVince002@users.noreply.github.com>
2023-05-02 00:09:38 +0200

    Utolsó simítások

--- alkalomadtan/app/tests.py
+++ alkalomadtan/app/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase, Client
-
+from django.test import TestCase, Client, tag
+from django.urls import reverse
 # Create your tests here.
+# megjelenik e rendesen e a főoldal?
+# chatGPT tanácsolása
+class HomepageViewTestCase(TestCase):
+    @tag("core")
+    def test_homepage_view(self):
+        # Make a GET request to the homepage URL
+        response = self.client.get(reverse("Kezdolap"))
+
+        # Check that the response status code is 200 OK
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the response contains the expected text
+        self.assertContains(response, "Kezdőlap")
+
+        # Check that the response uses the correct template
+        self.assertTemplateUsed(response, "app/index.html")
+
